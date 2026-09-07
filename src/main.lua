3b91d4a849bff928f7baba509d958c837793e57c
grievingflamesrp <memorysareapart@gmail.com>
2026-09-07 21:10:34 +0100

    Change game script URL to new repository
    Updated the URL for fetching game scripts to a new repository.

--- src/main.lua
+++ src/main.lua
@@ -96,7 +96,7 @@ if not shared.VapeIndependent then
 	else
 		if not shared.VapeDeveloper then
 			local suc, res = pcall(function()
-				return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeCompiled/'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
+				return game:HttpGet('https://github.com/grievingflamesrp/VapeCompiled/blob/main/main.lua'..readfile('newvape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
 				loadstring(downloadFile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
